9574ae3b5d4a5e672b239a40dc02521d72d0d006
zhsirong <zhsirong@umich.edu>
2025-04-11 14:02:53 -0400

    deploy build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/smartrecipe2/', // same as my repo name
   plugins: [react()],
-})
+});
